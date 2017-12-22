dfe6b2f977aa9d4534fefe8f5d36b7f91934a25a
Yusuke Yamamoto <yusuke@mac.com>
2017-12-22 16:04:45 +0900

    ガッ

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -3,6 +3,8 @@ import static mypackage.HelloWorld.*;
 
 public class Main {
     public static void main(String... args) {
+        String hello = null;
+        System.out.println(hello.toString());
         System.out.println(getHello());
     }
 }
