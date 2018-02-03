701025205bac72b197a65a430c148ff6f77b41e6
Yusuke Yamamoto <yusuke@mac.com>
2018-02-03 16:29:12 +0900

    hofgew

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -3,6 +3,6 @@ import static mypackage.HelloWorld.*;
 
 public class Main {
     public static void main(String... args) {
-        System.out.println(getHello());
+        System.out.println(getHello()+"hogehoge");
     }
 }
