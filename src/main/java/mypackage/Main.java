2b26aaf655eb17669501103c01332d9086ab7a09
Yusuke Yamamoto <yusuke@mac.com>
2018-05-10 16:27:38 +0900

    えいや！

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -3,6 +3,12 @@ import static mypackage.HelloWorld.*;
 
 public class Main {
     public static void main(String... args) {
+        String hello = null;
+        System.out.println(hello.toString());
+
+        String world = "hello world";
+        world = "hello jetbrains";
+        System.out.println(world);
         System.out.println(getHello());
     }
 }
