301c4487b530f64f547300fc0498889552d903cf
Yusuke Yamamoto <yusuke@mac.com>
2018-05-15 19:28:18 +0900

    getHello()でリファクタリング

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -3,6 +3,8 @@ import static mypackage.HelloWorld.*;
 
 public class Main {
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        System.out.println(getHello());
     }
 }
+// ssid: Agora
+// passowrd: StandUp.SpeakOut
