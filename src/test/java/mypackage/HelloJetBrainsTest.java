5d70dda43971f60a7aa75509a2159ab2bf14b532
Yusuke Yamamoto <yusuke@mac.com>
2018-05-26 12:40:17 +0900

    スリープ入れた

--- src/test/java/mypackage/HelloJetBrainsTest.java
+++ src/test/java/mypackage/HelloJetBrainsTest.java
@@ -7,5 +7,10 @@ public class HelloJetBrainsTest {
     @org.junit.Test
     public void getHello() {
         assertEquals("Hello JetBrains!", HelloJetBrains.getHello());
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
