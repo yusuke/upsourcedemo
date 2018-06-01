e62cebb2062e7a3e3376d8b11d5bae6581f3bf76
Yusuke Yamamoto <yusuke@mac.com>
2018-06-01 15:33:09 +0900

    爆弾仕掛けた

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -3,6 +3,9 @@ import static mypackage.HelloWorld.*;
 
 public class Main {
     public static void main(String... args) {
+
+        String hoge = null;
+        System.out.println(hoge.contains("hello"));
         System.out.println(getHello());
     }
 }
