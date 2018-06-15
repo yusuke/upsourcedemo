e0550b7f234f753d3a8cdc5076870900ad108238
Yusuke Yamamoto <yusuke@mac.com>
2018-06-15 19:27:26 +0900

    テストケース側を修正しちゃったﾃﾍﾍﾟﾛ

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -3,6 +3,8 @@ import static mypackage.HelloWorld.*;
 
 public class Main {
     public static void main(String... args) {
+        String hoge = null;
+        System.out.println(hoge.toString());
         System.out.println(getHello());
     }
 }
