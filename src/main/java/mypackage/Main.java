5e9e4e6f3a469cec4a2881b465e181d1fcdef90a
Yusuke Yamamoto <yusuke@mac.com>
2018-08-03 10:38:58 +0900

    getHello()でリファクタリング

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -7,6 +7,12 @@ import static mypackage.HelloWorld.*;
 
 public class Main {
     public static void main(String... args) {
+        String hello = null;
+        hello = "hello world";
+        if (hello.contains("hello")) {
+            System.out.println("hello");
+        }
+
         System.out.println(getHello());
     }
 }
