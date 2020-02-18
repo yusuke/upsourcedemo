8f005c34fc7e8ecc342dccbf84f8cd8adb08c4ba
Yusuke Yamamoto <yusuke@mac.com>
2020-02-18 17:44:55 +0900

    zulu

--- src/test/java/mypackage/HelloJetBrainsTest.java
+++ src/test/java/mypackage/HelloJetBrainsTest.java
@@ -1,11 +1,22 @@
 package mypackage;
 
+import java.util.Enumeration;
+import java.util.Properties;
+
 import static org.junit.Assert.*;
 
 public class HelloJetBrainsTest {
 
     @org.junit.Test
     public void getHello() {
+        System.out.println("----------------------");
+        final Properties properties = System.getProperties();
+        final Enumeration<Object> keys = properties.keys();
+        while (keys.hasMoreElements()) {
+            String key = (String) keys.nextElement();
+            System.out.println(key + ":" + System.getProperty(key));
+        }
+        System.out.println("----------------------");
         try {
             Thread.sleep(3);
         } catch (InterruptedException e) {
